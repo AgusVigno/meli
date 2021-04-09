8e898877eb78c9f852abc3a85c3e32415efb7eb1
Agustin Vignolo <agus88lp@gmail.com>
2021-04-09 11:24:12 -0300

    agregar eslint para validar el codigo

---
+++ client/.eslintrc.js
@@ -0,0 +1,35 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'react/prop-types': RULES.OFF,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
