257d85391d3a78fa5a909f6bbcc306ef6617a13a
Agustin Vignolo <agus88lp@gmail.com>
2021-04-10 17:56:25 -0300

    update package.json

--- client/store/store.js
+++ client/store/store.js
@@ -7,7 +7,4 @@ const composeEnhancer =
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose;
 
-export const store = createStore(
-  reducer,
-  composeEnhancer(applyMiddleware(thunk))
-);
+export const store = createStore(reducer, compose(applyMiddleware(thunk)));
