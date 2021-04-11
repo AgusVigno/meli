bd635b4279ee4fccc42df56863b04d52f4ab701d
Agustin Vignolo <agus88lp@gmail.com>
2021-04-11 11:37:35 -0300

    add constants

---
+++ server/constants/config.js
@@ -0,0 +1,9 @@
+const PORT = process.env.PORT || '4000';
+const API_BASE_URL = 'https://api.mercadolibre.com/';
+const LIMIT_SEARCH = 4;
+
+module.exports = {
+  PORT,
+  API_BASE_URL,
+  LIMIT_SEARCH,
+};
