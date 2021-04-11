957c6cdc0c78bf503e870826c14f89bcca785db6
Agustin Vignolo <agus88lp@gmail.com>
2021-04-11 11:42:34 -0300

    fix in NumberFormat with jest

--- client/utils/helpers.js
+++ client/utils/helpers.js
@@ -2,6 +2,7 @@ export const priceFormat = (value) => {
   const newNumber = new Intl.NumberFormat('es-AR', {
     style: 'currency',
     currency: value.currency,
+    minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(value.amount);
   return newNumber.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
