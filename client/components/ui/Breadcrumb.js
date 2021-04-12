3ab18b085bd9e326cbb4cf6c8206b4cd7922661c
Agustin Vignolo <agus88lp@gmail.com>
2021-04-12 17:57:44 -0300

    fix when load component breadcrumb

--- client/components/ui/Breadcrumb.js
+++ client/components/ui/Breadcrumb.js
@@ -4,12 +4,13 @@ import styles from '../../styles/items.module.scss';
 const Breadcrumb = ({ categories }) => {
   return (
     <ul className={styles.categories}>
-      {categories.map((category, index) => (
-        <li key={index} data-testid="breadcrumb">
-          <p>{category}</p>
-          {index < categories.length - 1 && <span>&nbsp; &#62; &nbsp;</span>}
-        </li>
-      ))}
+      {categories &&
+        categories.map((category, index) => (
+          <li key={index} data-testid="breadcrumb">
+            <p>{category}</p>
+            {index < categories.length - 1 && <span>&nbsp; &#62; &nbsp;</span>}
+          </li>
+        ))}
     </ul>
   );
 };
