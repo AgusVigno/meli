4d4301a12238757f4e7ba67309e6aa431036fe62
Agustin Vignolo <agus88lp@gmail.com>
2021-04-12 17:53:30 -0300

    update Breadcrumb component

--- client/components/ui/Breadcrumb.js
+++ client/components/ui/Breadcrumb.js
@@ -1,29 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from '../../styles/items.module.scss';
 
 const Breadcrumb = ({ categories }) => {
-  const [breadcrumb, setBreadcrumb] = useState('');
-
-  useEffect(() => {
-    if (categories) {
-      if (typeof categories === 'string') {
-        setBreadcrumb(categories);
-      } else {
-        let text = '';
-        for (let index = 0; index < categories.length; index++) {
-          text += `${categories[index]} ${
-            index < categories.length - 1 ? ' > ' : ''
-          }`;
-        }
-        setBreadcrumb(text);
-      }
-    }
-  }, [categories]);
-
   return (
-    <p data-testid="breadcrumb" className={styles.category}>
-      {breadcrumb}
-    </p>
+    <ul className={styles.categories}>
+      {categories.map((category, index) => (
+        <li key={index} data-testid="breadcrumb">
+          <p>{category}</p>
+          {index < categories.length - 1 && <span>&nbsp; &#62; &nbsp;</span>}
+        </li>
+      ))}
+    </ul>
   );
 };
 
